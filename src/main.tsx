e46cc92d43435e52a9b172ed559e12f13c08cda4
amanimoulai16-eng <amanimoulai16@gmail.com>
2026-09-08 20:34:04 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { registerSW } from 'virtual:pwa-register';
 
 registerSW({ immediate: true });
-  
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
